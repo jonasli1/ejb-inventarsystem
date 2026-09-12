cf0d8f76d183c3764bcf96325dcd5be17d3b269b
Jonas Lindner <68064542+jonasli1@users.noreply.github.com>
2026-09-12 17:00:50 +0200

    ios app initial

---
+++ ios/Inventarsystem/Inventarsystem/InventarsystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventarsystemApp.swift
+//  Inventarsystem
+//
+//  Created by Jonas on 12.09.26.
+//
+
+import SwiftUI
+
+@main
+struct InventarsystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
